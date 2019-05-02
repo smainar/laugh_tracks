79d5fcf8cdf125edffc2892d2285537a776031ed
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-02 12:58:34 -0600

    Edit relationships to show that comedian should have_many :specials

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Special, type: :model do
+RSpec.describe Special, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
   end
