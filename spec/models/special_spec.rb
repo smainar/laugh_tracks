d61c467e26545863795c64ce6b1ef478baca4979
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-02 15:45:04 -0600

    Add validate_presence_of :runtime in special_spec.rb

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Special, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :runtime }
   end
 
   describe 'relationships' do
