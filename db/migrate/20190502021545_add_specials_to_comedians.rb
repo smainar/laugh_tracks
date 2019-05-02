57b277723afa8ff7fa3f919faa1921b759b0db9d
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-02 12:51:01 -0600

    Add migration specials to comedians

--- db/migrate/20190502021545_add_specials_to_comedians.rb
+++ db/migrate/20190502021545_add_specials_to_comedians.rb
@@ -1,5 +1,5 @@
 class AddSpecialsToComedians < ActiveRecord::Migration[5.1]
   def change
-    add_reference :comedians, :special, foreign_key: true
+    add_reference :specials, :comedian, foreign_key: true
   end
 end
