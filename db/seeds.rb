a16a3a3dca4e697311620ab9c30957d374c55309
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-03 17:44:58 -0600

    Add seed data for comedians

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+c1 = Comedian.create(name: 'Demetri Martin', age: 45, hometown: 'New York City', image: 'https://m.media-amazon.com/images/M/MV5BMjA3Nzg5MzM2Nl5BMl5BanBnXkFtZTcwODYzOTAzMg@@._V1_UY317_CR20,0,214,317_AL_.jpg')
+c2 = Comedian.create(name: 'Steve Martin', age: 73, hometown: 'Waco', image: 'https://m.media-amazon.com/images/M/MV5BNDY0ODYwNDM3OV5BMl5BanBnXkFtZTcwMTc3NjQzMg@@._V1_UX214_CR0,0,214,317_AL_.jpg')
+c3 = Comedian.create(name: 'Trevor Noah', age: 35, hometown: 'Johannesburg', image: 'https://m.media-amazon.com/images/M/MV5BMjQ0MjEzNjczMF5BMl5BanBnXkFtZTgwNDEzNTY4NzE@._V1_UY317_CR12,0,214,317_AL_.jpg')
+c4 = Comedian.create(name: 'Ellen Degeneres', age: 60, hometown: 'Metairie', image: 'https://m.media-amazon.com/images/M/MV5BNDAwMzAyNDEzMV5BMl5BanBnXkFtZTcwMDU3MTAxMw@@._V1_UY317_CR12,0,214,317_AL_.jpg')
+c5 = Comedian.create(name: 'John Mulaney', age: 36, hometown: 'Chicago', image: 'https://i.pinimg.com/736x/07/31/7d/07317d772dd1e93e1b99c2567da65735.jpg')
+c6 = Comedian.create(name: 'George Carlin', age: 71, hometown: 'New York City', image: 'https://m.media-amazon.com/images/M/MV5BMTI4ODgzNTk0NV5BMl5BanBnXkFtZTcwNjU1MjY3MQ@@._V1_UY317_CR11,0,214,317_AL_.jpg')
+c7 = Comedian.create(name: 'Martin Short', age: 69, hometown: 'Hamilton', image: 'https://m.media-amazon.com/images/M/MV5BMTY1MDIyMDY1N15BMl5BanBnXkFtZTgwNzMzMDE5NTE@._V1_UX214_CR0,0,214,317_AL_.jpg')
+c8 = Comedian.create(name: 'Chris Rock', age: 54, hometown: 'Andrews', image: 'https://m.media-amazon.com/images/M/MV5BMTEyNjM5MjgyNzdeQTJeQWpwZ15BbWU3MDAzMzUyODc@._V1.._UX214_CR0,0,214,317_AL_.jpg')
+c9 = Comedian.create(name: 'Bill Burr', age: 51, hometown: 'Canton', image: 'https://m.media-amazon.com/images/M/MV5BMTQwNDgwODg3NV5BMl5BanBnXkFtZTcwMTY5OTM3MQ@@._V1_UY317_CR10,0,214,317_AL_.jpg')
+c10 = Comedian.create(name: 'Chris Hardwick', age: 48, hometown: 'Louisville', image: 'https://m.media-amazon.com/images/M/MV5BMGRiN2Q4ZWQtMjdiZS00ZWY1LWEyNWMtMzliYjRkZWE3ODEzXkEyXkFqcGdeQXVyMTkxNjA2OTI@._V1_UX214_CR0,0,214,317_AL_.jpg')
+c11 = Comedian.create(name: 'Jerry Seinfeld', age: 65, hometown: 'Brooklyn', image: 'https://m.media-amazon.com/images/M/MV5BMTYxMDkwMjM5NV5BMl5BanBnXkFtZTcwMzUxMTgxNw@@._V1_UX214_CR0,0,214,317_AL_.jpg')
+c12 = Comedian.create(name: 'Margaret Cho', age: 65, hometown: 'San Francisco', image: 'https://m.media-amazon.com/images/M/MV5BZTAxYmU1ODItYWViNC00YWJiLWJiYTEtNzExYTk3MmE4NDZkXkEyXkFqcGdeQXVyNTk0ODYyMTk@._V1_UX214_CR0,0,214,317_AL_.jpg')
