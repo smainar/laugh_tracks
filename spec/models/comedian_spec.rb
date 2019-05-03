445dd810bba8cbcfcc0b6faba2cd100cccbccb41
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-02 22:25:35 -0600

    Add class methods test for .find_age in comedian_spec.rb

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -7,7 +7,22 @@ RSpec.describe Comedian, type: :model do
     it { should validate_presence_of :hometown }
   end
 
-  describe 'relationships' do
+  describe "relationships" do
     it { should have_many :specials }
+  end
+
+  describe "class methods" do
+    before(:each) do
+      @comedian_1 = Comedian.create(name: 'Demetri Martin', age: 45, hometown: 'New York City', image: 'https://m.media-amazon.com/images/M/MV5BMjA3Nzg5MzM2Nl5BMl5BanBnXkFtZTcwODYzOTAzMg@@._V1_UY317_CR20,0,214,317_AL_.jpg')
+      @comedian_2 = Comedian.create(name: 'Steve Martin', age: 73, hometown: 'Waco', image: 'https://m.media-amazon.com/images/M/MV5BNDY0ODYwNDM3OV5BMl5BanBnXkFtZTcwMTc3NjQzMg@@._V1_UX214_CR0,0,214,317_AL_.jpg')
+      @comedian_3 = Comedian.create(name: 'Bob', age: 45, hometown: 'New York City', image: 'https://m.media-amazon.com/images/M/MV5BMjA3Nzg5MzM2Nl5BMl5BanBnXkFtZTcwODYzOTAzMg@@._V1_UY317_CR20,0,214,317_AL_.jpg')
+      @comedian_4 = Comedian.create(name: 'Sally', age: 73, hometown: 'Waco', image: 'https://m.media-amazon.com/images/M/MV5BNDY0ODYwNDM3OV5BMl5BanBnXkFtZTcwMTc3NjQzMg@@._V1_UX214_CR0,0,214,317_AL_.jpg')
+    end
+
+    it ".find_age" do
+      age = 45
+
+      expect(Comedian.all.find_age(age)).to eq([@comedian_1, @comedian_3])
+    end
   end
 end
