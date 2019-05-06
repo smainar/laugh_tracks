e8e2c07fd32ee22280955acdadba99fb8b86f608
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-06 06:50:24 -0600

    Delete comments from test in comedians/new_spec.rb

--- spec/features/comedians/new_spec.rb
+++ spec/features/comedians/new_spec.rb
@@ -1,13 +1,5 @@
 require "rails_helper"
 
-# As a visitor
-# When I visit `/comedians/new`
-# Then I see a form to input a new comedian into the database
-# Including fields for their name, age and city.
-# When the form is successfully submitted and saved,
-# Then I am redirected to `/comedians`
-# And I see the new comedian's data on the page.
-
 RSpec.describe "As a vistor", type: :feature do
   context "When I visit `/comedians/new`" do
     it "I can fill out a form to create a new comedian" do
