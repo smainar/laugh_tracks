da621b8d4fb06bfe3f027e1af0f618a8fb3eb0bf
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-06 05:15:05 -0600

    Refactor validates_presence_of in comedian model

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,9 +1,9 @@
 class Comedian < ApplicationRecord
   has_many :specials
 
-  validates_presence_of :name
-  validates_presence_of :age
-  validates_presence_of :hometown
+  validates_presence_of :name,
+                        :age,
+                        :hometown
 
   def self.find_age(age)
     self.where(age: age)
